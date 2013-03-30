94eb4bd6baad71bd25ced878d54dc31f871100f2
Du Kai <dande618@qq.com>
2013-03-30 08:28:01 +0800

    add about activity

---
+++ qingwei/src/com/argon/wenfeng/activity/MyCard.java
@@ -0,0 +1,25 @@
+package com.argon.wenfeng.activity;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+import com.argon.wenfeng.R;
+import com.fima.cardsui.objects.Card;
+
+public class MyCard extends Card {
+
+	public MyCard(String title,String desc,int image){
+		super(title,desc,image);
+	}
+
+	@Override
+	public View getCardContent(Context context) {
+		View view= LayoutInflater.from(context).inflate(R.layout.card_ex, null);
+		((TextView) view.findViewById(R.id.title)).setText(title);
+		((TextView) view.findViewById(R.id.description)).setText(desc);
+		return view;
+	}
+
+}
