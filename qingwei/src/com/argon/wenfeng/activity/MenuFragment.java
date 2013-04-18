bd8441488830ae2cdd504c5d46b0a52a7dac8519
pengxiao <pengxiao.du@gmail.com>
2013-04-18 13:42:28 +0800

    add sidemenu support

---
+++ qingwei/src/com/argon/wenfeng/activity/MenuFragment.java
@@ -0,0 +1,75 @@
+package com.argon.wenfeng.activity;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.argon.wenfeng.R;
+
+/**
+ * A fragment representing a list of Items.
+ * <p />
+ * <p />
+ * Activities containing this fragment MUST implement the {@link Callbacks}
+ * interface.
+ */
+public class MenuFragment extends ListFragment {
+
+	private Context mContext;
+	
+	/**
+	 * Mandatory empty constructor for the fragment manager to instantiate the
+	 * fragment (e.g. upon screen orientation changes).
+	 */
+	public MenuFragment(Context context) {
+		mContext = context;
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.list, null);
+	}
+	
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		String[] colors = getResources().getStringArray(R.array.menu_items);
+		ArrayAdapter<String> colorAdapter = new ArrayAdapter<String>(getActivity(), 
+				android.R.layout.simple_list_item_1, android.R.id.text1, colors);
+		setListAdapter(colorAdapter);
+	}
+
+	@Override
+	public void onListItemClick(ListView lv, View v, int position, long id) {
+		Fragment newContent = null;
+		switch (position) {
+		case 0:
+			newContent = new MainFragment(getActivity());
+			break;
+		}
+		if (newContent != null)
+			switchFragment(newContent);
+	}
+	
+	// the meat of switching the above fragment
+	private void switchFragment(Fragment fragment) {
+		if (getActivity() == null)
+			return;
+		if (getActivity() instanceof MainActivity) {
+			MainActivity mainAcitivity = (MainActivity) getActivity();
+			mainAcitivity.switchContent(fragment);
+		}	
+	}
+}
